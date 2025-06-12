85698d0110c0d27792a92843d4337cab3c01b192
Cloudflipper <123280088+Cloudflipper@users.noreply.github.com>
2025-06-12 17:28:00 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -25,6 +25,14 @@ sudo apt install htop -y &&\  #htop install
 sudo apt install python3-pip libopenblas-base libopenmpi-dev &&\  # pip install
 
 #PyTorch Install
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600^C
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+
+
+sudo apt install cuda-toolkit-11-4
+sudo apt install libcudnn8 libcudnn8-dev
 
 wget https://developer.download.nvidia.com/compute/redist/jp/v512/pytorch/torch-2.1.0a0+41361538.nv23.06-cp38-cp38-linux_aarch64.whl &&\  
 sudo apt install libopenmpi-dev libjpeg-dev zlib1g-dev libpython3-dev libopenblas-dev  &&\  
@@ -58,4 +66,4 @@ sudo apt install -y python3-rosinstall python3-rosinstall-generator python3-wsto
 
 #tqdm and moviepy
 pip3 install tqdm &&\
-pip3 install "moviepy<2.0.0" &&\
+pip3 install "moviepy<2.0.0"
